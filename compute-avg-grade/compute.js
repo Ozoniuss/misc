a8da1a67642a843c2011120ee4931ca356f0a125
ozoniuss <alexbledea_2000@yahoo.com>
2023-02-17 00:05:12 +0200

    feat(compute-avg-grade): generalize and update for master's

--- compute-avg-grade/compute.js
+++ compute-avg-grade/compute.js
@@ -1,14 +1,33 @@
-// Get all rows of the table.
-const rows = document.querySelectorAll(".table-striped")[1].rows
+// Get all rows of the table. Use 1 as the index for bachelor's.
+const rows = document.querySelectorAll(".table-striped")[0].rows
 
 // Total sum and total number of credits.
 let sum = 0
 let nr_credits = 0
 
+header = rows[0]
+
+let grade_pos = 0
+let credit_pos = 0
+
+for (let i = 0; i < header.cells.length; i++) {
+    if (header.cells[i].innerHTML === " Nota") {
+        grade_pos = i
+    }
+    if (header.cells[i].innerHTML === "Nr. Credite") {
+        credit_pos = i
+    }
+}
+
 rows.forEach((row, index) => {
 
-    grade = row.cells[5].innerText
-    credit = row.cells[6].innerText
+    // header
+    if (index === 0) {
+        return
+    }
+
+    grade = row.cells[grade_pos].innerText
+    credit = row.cells[credit_pos].innerText
 
     // Some subjects don't have grades, can be "passed" or "failed", or simply
     // not computed yet. The grade may also be set to the empty string, which
