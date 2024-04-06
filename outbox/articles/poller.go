1706262e65794ef5dd640e90aef505e0695fe3b7
Alex Bledea <qscxzsewqazxcde@gmail.com>
2024-04-06 23:04:45 +0300

    fix deadline issue

--- outbox/articles/poller.go
+++ outbox/articles/poller.go
@@ -21,9 +21,6 @@ type LikedArticlesPoller struct {
 
 	conn net.Conn
 
-	enc *json.Encoder
-	dec *json.Decoder
-
 	// Do not send new events until this specific one is acked. This also
 	// means not fetching any new events until that point.
 	unacked        Message
@@ -50,10 +47,8 @@ func NewLikedArticlesPoller(storage ArticleStorage, pollInterval time.Duration, 
 		return nil, fmt.Errorf("could not connect to downstream: %s", err.Error())
 	}
 
-	enc := json.NewEncoder(conn)
-	dec := json.NewDecoder(conn)
-
 	timeout := pollInterval - 1*time.Second
+	log.Info().Any("timeout", timeout.Seconds()).Msg("timeout")
 
 	return &LikedArticlesPoller{
 		storage:        storage,
@@ -62,8 +57,6 @@ func NewLikedArticlesPoller(storage ArticleStorage, pollInterval time.Duration, 
 		timeout:        timeout,
 		conn:           conn,
 		lastEventAcked: true,
-		enc:            enc,
-		dec:            dec,
 		unacked:        Message{},
 	}, nil
 }
@@ -74,8 +67,8 @@ func (p *LikedArticlesPoller) Poll() {
 		cycle := 0
 		for {
 			cycle++
-			log.Info().Int("cycle", cycle).Msg("starting new polling cycle")
 			<-ticker.C
+			log.Info().Int("cycle", cycle).Msg("starting new polling cycle")
 
 			newEvents, err := p.storage.GetArticleLikedEventsFromIndex(p.lastEventIndex)
 
@@ -127,7 +120,8 @@ func (p *LikedArticlesPoller) sendNewEvents(events []ArticleLikedEvent) error {
 		}
 	}
 
-	err := p.enc.Encode(message)
+	enc := json.NewEncoder(p.conn)
+	err := enc.Encode(message)
 	if err != nil {
 		return fmt.Errorf("could not send events: %s", err.Error())
 	}
@@ -137,10 +131,15 @@ func (p *LikedArticlesPoller) sendNewEvents(events []ArticleLikedEvent) error {
 
 	// If the ack is not received after the timeout, we consider that the
 	// consumer failed.
-	p.conn.SetReadDeadline(time.Now().Add(p.timeout))
+	deadline := time.Now().Add(p.timeout)
+	err = p.conn.SetReadDeadline(deadline)
+	if err != nil {
+		log.Error().Err(err).Msg("setting deadline")
+	}
+	log.Info().Any("deadline", deadline).Msg("deadline set")
 
 	var ack Ack
-	err = p.dec.Decode(&ack)
+	dec := json.NewDecoder(p.conn)
 
 	// Note that if we sent the same event multiple times, it doesn't matter
 	// that the ACK may be delayed, nor that we may receive multiple ACKs.
@@ -153,11 +152,13 @@ func (p *LikedArticlesPoller) sendNewEvents(events []ArticleLikedEvent) error {
 	var loopErr error
 LOOP:
 	for {
+		err := dec.Decode(&ack)
 		// this error includes timeout
 		if err != nil {
 			var e *net.OpError // internal net error
 			ok := errors.As(err, &e)
 			if !ok {
+				log.Error().Err(err).Msg("got unknown error")
 				// this covers all errors, including decoding errors. It means
 				// the event did not get ACKed properly.
 				p.lastEventAcked = false
@@ -165,6 +166,7 @@ LOOP:
 				loopErr = err
 			} else {
 				if e.Timeout() {
+					log.Error().Err(err).Msg("consumer timed out")
 					// This also counts as not being acked, especially the first
 					// time. The difference here is that we're breaking the
 					// loop.
@@ -196,7 +198,7 @@ LOOP:
 	}
 
 	if loopErr != nil {
-		return fmt.Errorf("failed to ack; last received error: %s", err.Error())
+		return fmt.Errorf("failed to ack; last received error: %s", loopErr.Error())
 	}
 	return nil
 }
--- outbox/main.py
+++ outbox/main.py
@@ -1,9 +1,25 @@
 import datetime
 import json
 import socket
+from time import sleep
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
 from dataclasses import dataclass, asdict
 
+
+
+# #define data
+# df = pd.DataFrame({'date': np.array([datetime.datetime(2020, 1, i+1)
+#                                      for i in range(12)]),
+#                    'sales': [3, 4, 4, 7, 8, 9, 14, 17, 12, 8, 8, 13]})
+
+# #plot time series
+# plt.plot(df.date, df.sales, linewidth=3)
+# plt.savefig("data.png")
+
 HOST = "127.0.0.1"  
+
 PORT = 13311
 
 @dataclass
@@ -30,13 +46,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     with conn:
         print(f"Connected by {addr}")
+        c = 0
         while True:
             try:
                 data = conn.recv(4096).decode('UTF-8')
+                print("received data", data)
                 message = Message(**json.loads(str(data)))
                 ack = json.dumps(asdict(Ack(id=message.id)))
                 receivedEvents.extend(message.events)
-                conn.sendall(bytes(ack,encoding="utf-8"))
+                # sleep(7)
+                c += 1
+                if c == 3:
+                    print("sent")
+                    conn.sendall(bytes(ack,encoding="utf-8"))
 
             except Exception as e:
                 print("exception", e)
